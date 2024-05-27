0939abe1d06f4c557b72232f2b5ba25fe58736cb
daguttt <dagutmu667@gmail.com>
2024-05-27 11:26:33 -0500

    wip feat: add markup and styles for register scene

--- app/scenes/public/register/Register.js
+++ app/scenes/public/register/Register.js
@@ -1,6 +1,8 @@
 export function RegisterScene({ searchParams }) {
-  const root = document.getElementById('root');
-  root.innerHTML = `
+  const html = /*html*/ `
     <h1>Register</h1>
   `;
+
+  const logic = () => {};
+  return { html, logic };
 }
