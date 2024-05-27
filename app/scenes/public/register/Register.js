4b0bf86e756c5f965765b123938a2b52baf51bbb
daguttt <dagutmu667@gmail.com>
2024-05-27 13:31:28 -0500

    chore: add demo register scene

--- app/scenes/public/register/Register.js
+++ app/scenes/public/register/Register.js
@@ -1,8 +1,28 @@
+import { navigateTo } from '../../../Router';
+
+import globalStyles from '../../../styles/global.css';
+import styles from './Register.css';
+
 export function RegisterScene({ searchParams }) {
   const html = /*html*/ `
-    <h1>Register</h1>
+      <div class="${globalStyles.container} ${styles.container}">
+        <header>
+          <h1>Registro</h1>
+        </header>
+        <main>
+          <h2>Coming soon!</h2>
+          <p>Estamos preparando esta sección.</p>
+          <button id="btn-login">Volver al login</button>
+        </main>
+      </div>
   `;
 
-  const logic = () => {};
+  const logic = () => {
+    const $loginBtn = document.getElementById('btn-login');
+    if (!$loginBtn) throw new Error('Login button not found');
+    $loginBtn.addEventListener('click', () => {
+      navigateTo('/login');
+    });
+  };
   return { html, logic };
 }
