ad31e2dc17d3c21f61e8716a469cb57f3c93e76f
Mahi_G13 <73084766+MahicolGallego@users.noreply.github.com>
2024-05-28 15:42:55 -0500

    add page not found (#3)

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 import { HomeScene } from './scenes/private/home';
-
 // import { RootScene } from './scenes/public/root';
+import { PathNotFoundScene } from './scenes/public/path-not-found';
 import { LoginScene } from './scenes/public/login';
 import { RegisterScene } from './scenes/public/register';
 import { ComponetsUsageScene } from './scenes/public/components-usage';
@@ -12,5 +12,6 @@ export const routes = {
     { path: '/login', component: LoginScene },
     { path: '/register', component: RegisterScene },
     { path: '/components-usage', component: ComponetsUsageScene },
+    { path: '/not-found', component: PathNotFoundScene },
   ],
 };
