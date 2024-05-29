97ac24794d91161dec00f3e27af9a252f597cb71
Mahi_G13 <maicolgallego14@gmail.com>
2024-05-28 23:07:01 -0500

    WIP: ADD Type user validate and create switch structure for different views and Add several constants

---
+++ app/constants/userRolesEnum.js
@@ -0,0 +1,9 @@
+export function getUserRoles() {
+  const userRols = {
+    admin: 1,
+    student: 2,
+    instructor: 3,
+  };
+
+  return userRols;
+}
--- app/scenes/private/home/Home.js
+++ app/scenes/private/home/Home.js
@@ -1,3 +1,4 @@
+import { getUserRoles } from '../../../constants';
 import { logOut } from '../../../helpers/log-out';
 
 import globalStyles from '../../../styles/global.css';
@@ -8,29 +9,31 @@ export function HomeScene({ searchParams }) {
   if (!userFromStorage) {
     console.error('User not found in localStorage');
   }
-
   const user = JSON.parse(userFromStorage);
 
-  const html = /*html*/ `
-    <div class="${globalStyles.container} ${styles.container}">
-      <header>
-        <h1>¡Hola, <span class="${styles.username}">${user.name}</span>!</h1>
-        <button id="btn-logout">Cerrar sesión</button>
-      </header>
-      <main>
-        <h2>Coming soon!</h2>
-        <p>Estamos preparando esta sección.</p>
-      </main>
-    </div>
-  `;
+  console.log(user);
 
-  const logic = () => {
-    const $logOutBtn = document.getElementById('btn-logout');
-    if (!$logOutBtn) throw new Error('Log out button not found');
-    $logOutBtn.addEventListener('click', () => {
-      logOut();
-    });
-  };
+  const userRoles = getUserRoles();
+  console.log(userRoles);
+
+  let logic;
+  let html;
+  // user.user_roles_id = 3; prueba
+
+  switch (user.user_roles_id) {
+    case userRoles.admin:
+      html = /*html*/ ``;
+      logic = () => {};
+      break;
+    case userRoles.student:
+      html = /*html*/ ``;
+      logic = () => {};
+      break;
+    case userRoles.instructor:
+      html = /*html*/ ``;
+      logic = () => {};
+      break;
+  }
 
   return {
     html,
