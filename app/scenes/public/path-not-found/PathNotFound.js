2de29412e802f1537b540d84935e88ec10672d3d
daguttt <dagutmu667@gmail.com>
2024-05-29 09:40:06 -0500

    fix: redirect from 404 page to home

--- app/scenes/public/path-not-found/PathNotFound.js
+++ app/scenes/public/path-not-found/PathNotFound.js
@@ -11,7 +11,7 @@ export function PathNotFoundScene() {
   const logic = () => {
     const $buttonGetBack = document.getElementById(`btn_getBack`);
     $buttonGetBack.addEventListener('click', () => {
-      navigateTo('/');
+      navigateTo('/login');
     });
   };
   return { html, logic };
